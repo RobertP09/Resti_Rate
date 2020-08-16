dedbc1c4879409dc25c853dfc8fdd4d49bcf7231
Robert Perez <robgaray9111@yahoo.com>
2020-08-16 11:56:22 -0700

    Added client - Working on base layout and components

---
+++ client/src/components/RestaurantList.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const RestaurantList = () => {
+	return (
+		<div>
+			<table className='table table-dark'>
+				<thead>
+					<tr>
+						<th scope='col'>Restaurant</th>
+						<th scope='col'>Location</th>
+						<th scope='col'>Price Range</th>
+						<th scope='col'>Ratings</th>
+						<th scope='col'>Edit</th>
+						<th scope='col'>Delete</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>McD</td>
+						<td>Lompoc</td>
+						<td>$$$</td>
+						<td>Rating</td>
+						<td>
+							<button className='btn btn-warning'>Update</button>
+						</td>
+						<td>
+							<button className='btn btn-danger'>Delete</button>
+						</td>
+					</tr>
+					<tr>
+						<td>McD</td>
+						<td>Lompoc</td>
+						<td>$$$</td>
+						<td>Rating</td>
+						<td>
+							<button className='btn btn-warning'>Update</button>
+						</td>
+						<td>
+							<button className='btn btn-danger'>Delete</button>
+						</td>
+					</tr>
+					<tr>
+						<td>McD</td>
+						<td>Lompoc</td>
+						<td>$$$</td>
+						<td>Rating</td>
+						<td>
+							<button className='btn btn-warning'>Update</button>
+						</td>
+						<td>
+							<button className='btn btn-danger'>Delete</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	)
+}
+
+export default RestaurantList
