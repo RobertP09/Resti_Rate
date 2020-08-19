c4d4ff6b502e06dbba18464de7ca4c2c3f205174
Robert Perez <robgaray9111@yahoo.com>
2020-08-18 20:21:59 -0700

    Added logging and removed client console logs

--- server/server.js
+++ server/server.js
@@ -8,12 +8,18 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
 
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
 app.use(cors({ origin: 'http://localhost:3000' }))
-app.use(morgan("tiny"))
+app.use(morgan("common", { stream: accessLogStream }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/v1/restaurants', require('./routes/restaurants'))
+
+app.all('*', (req, res) => {
+    res.redirect('/api/v1/restaurants');
+})
 
 app.listen(PORT, () => {
     console.log(`Live @ http://localhost:${PORT}`);
